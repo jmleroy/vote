08cdc8d03b7557deef30eddb6ebed2953f927cd1
Jean-Marc Leroy <info@studio16.be>
2018-10-09 11:49:05 +0200

    Update communes.js

--- communes.js
+++ communes.js
@@ -1,4 +1,38 @@
 const communes = {
+	"Engis": {
+        maxVotes: 4545,
+        maxSeats: 17,
+        previousMaxVotes: 4235,
+        previousMaxSeats: 17,
+        lists: {
+            "Ecolo": {
+                backgroundColor: '#3AAA11',
+                textColor: 'white'
+            },
+            "EngiSolidair": {
+                backgroundColor: 'red',
+                textColor: 'white',
+                previousName: "PS",
+                previousVotes: 2260,
+                previousSeats: 13
+            },
+            "E+": {
+                backgroundColor: '#e8296f',
+                textColor: 'white',
+                previousName: "MR + cdH",
+                previousVotes: 1326,
+                previousSeats: 4
+            },
+            "MCER": {
+                backgroundColor: 'teal',
+                textColor: 'white'
+            },
+            "Parti Social": {
+                backgroundColor: 'crimson',
+                textColor: 'white'
+            }
+        }
+    },
     "Héron": {
         maxVotes: 3904,
         maxSeats: 17,
