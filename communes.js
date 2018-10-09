4fd8a21cf76e90bf3b498ecc118928d8fc7ce258
Jean-Marc Leroy <info@studio16.be>
2018-10-09 11:07:28 +0200

    adds communes data separately
    communes data are now provided separately

---
+++ communes.js
@@ -0,0 +1,38 @@
+const communes = {
+    "Héron": {
+        maxVotes: 3904,
+        maxSeats: 17,
+        previousMaxVotes: 3731,
+        previousMaxSeats: 15,
+        lists: {
+            "Ecolo OuVert": {
+                backgroundColor: '#3AAA11',
+                textColor: 'white',
+                previousName: "Ecolo",
+                previousVotes: 410,
+                previousSeats: 1
+            },
+            "EC": {
+                backgroundColor: 'teal',
+                textColor: 'white',
+                previousName: "Renouveau + IC",
+                previousVotes: 1443,
+                previousSeats: 6
+            },
+            "LB": {
+                backgroundColor: '#9F0122',
+                textColor: 'White',
+                previousName: "LB",
+                previousVotes: 1331,
+                previousSeats: 7
+            },
+            "Vous Héron": {
+                backgroundColor: 'Khaki',
+                textColor: 'Black',
+                previousName: "A.Cloes (LB)",
+                previousVotes: 74,
+                previousSeats: 1
+            }
+        }
+    }
+};
